409b4fa3e20e1a94d043d0729747940a17c2d433
jacobson <ajacobson50@gmail.com>
2015-02-25 19:07:04 -0500

    2/25/15

--- Tap Video For PictureTests/Tap_Video_For_PictureTests.m
+++ Tap Video For PictureTests/Tap_Video_For_PictureTests.m
@@ -5,7 +5,13 @@
 //  Created by Jacobson on 2/25/15.
 //  Copyright (c) 2015 Alex Jacobson. All rights reserved.
 //
-
+//  Software written for the expressed purpose of evaluating
+//  Alex Jacobson's software programing skills.
+//
+//  Any commercial use of this software without the expressed written
+//  concent of Alex Jacobson is strictly forbidden
+//
+// contact: ajacobson50@gmial.com
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
